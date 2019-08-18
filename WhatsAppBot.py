b5585f621781d5b0117047548858ac1369745599
tugceozgur <tugceoztetik@gmail.com>
2019-08-18 19:46:36 +0200

    comment changed

--- WhatsAppBot.py
+++ WhatsAppBot.py
@@ -1,44 +1,52 @@
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
-import csv
 
-#download chromedriver then add it to path using the following
+#how many times the message required to send will be specified here. 
+
+with open("references.dat", 'r') as mc:
+    message_count = mc.read()
+
+#used to write in message block and finding the users.
+ 
+with open("message.dat", 'r') as cm:
+    custom_message = cm.read()
+
+with open('filename.dat', 'r') as un: 
+    all_user_names = un.read().splitlines()
+    
+
+#USERDATA for adding to the cookeies automatically after scanning. 
+
 options = Options()
 options.add_argument("user-data-dir=~/USERDATA")
-driver = webdriver.Chrome(chrome_options=options,executable_path='./chromedriver')
 
+#user defines the path where chromedriver extracted. 
+
+driver = webdriver.Chrome(chrome_options = options, executable_path = '/home/tugce/WhatsappWebProject/WhatsappBot/chromedriver')
 driver.get('https://web.whatsapp.com/')
 
-#time duration between opening whatsapp Web and writing the message. 
+# suggested waiting time. 
+
 time.sleep(3)
-
-#tells how many times yo want to send the message. 
-message_count = 5
-
-#open and read the file which contains the message. 
-x = open("message.dat", 'r').read()
-
-with open('filename.dat', 'r') as user_name:
-    #reading names from file can both be performed with cvs or splitlines methods. 
-    #reader = csv.reader(f, dialect='excel', delimiter='\n')
-    all_user_names = user_name.read().splitlines()
     
-    #for each name, it will go to message space, and click there.
-    for row in all_user_names:
+'''
+    Procedure: 
 
-        user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(row))
-        user.click()
-        msg_box = driver.find_element_by_class_name('_3u328')
+    identifies the user name, finds the message box and button, writes the message and sends. 
+
+'''
+
+for row in all_user_names:
+
+    user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(row))
+    user.click()
+    msg_box = driver.find_element_by_class_name('_3u328')
+
+    for i in range(0,int(message_count)):
         
-        # take the message from the file and writes it into the empty message block and click the send button. 
-        for i in range(0,message_count):
-
-            msg_box.send_keys(x)
-            button = driver.find_element_by_class_name('hnQHL')
-            button.click()
+        msg_box.send_keys(custom_message)
+        button = driver.find_element_by_class_name('hnQHL')
+        button.click()
 
 driver.close()
